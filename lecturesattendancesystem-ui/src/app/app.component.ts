5041e4f41d59cd97dae9e44a15d2fd1069d84a96
elkprostoelk <55785891+elkprostoelk@users.noreply.github.com>
2022-04-16 02:28:20 +0300

    provided main design

--- lecturesattendancesystem-ui/src/app/app.component.ts
+++ lecturesattendancesystem-ui/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lecturesattendancesystem-ui';
+  date: Date;
+
+  constructor() {
+    this.date = new Date();
+  }
 }
