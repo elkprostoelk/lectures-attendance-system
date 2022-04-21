f983478ff82e8f2451c45dad4ceb2ff9de657122
elkprostoelk <55785891+elkprostoelk@users.noreply.github.com>
2022-04-22 01:52:39 +0300

    added auth guard and provided it for admin component

--- lecturesattendancesystem-ui/src/app/app-routing.module.ts
+++ lecturesattendancesystem-ui/src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from "./components/login/login.component";
 import {AdminComponent} from "./components/admin/admin.component";
+import {AuthGuard} from "./auth-guard/auth.guard";
 
 const routes: Routes = [
-  {path: "login", component: LoginComponent},
-  {path: "admin", component: AdminComponent}
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'admin', component: AdminComponent,
+    canActivate: [AuthGuard],
+    data: {
+      role: 'administrator'
+    }
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ lecturesattendancesystem-ui/src/app/auth-guard/auth.guard.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  CanDeactivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import {AuthService} from "../services/auth/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router) {
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let url: string = state.url;
+    return this.checkUserLogin(route);
+  }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.canActivate(childRoute, state);
+  }
+
+  canDeactivate(
+    component: unknown,
+    currentRoute: ActivatedRouteSnapshot,
+    currentState: RouterStateSnapshot,
+    nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+  checkUserLogin(route: ActivatedRouteSnapshot): boolean {
+    if (this.authService.isSignedIn()) {
+      const userRole = this.authService.parseJwt()?.role;
+      if (route.data['role'] && route.data['role'].indexOf(userRole) === -1) {
+        this.router.navigate(['/']);
+        return false;
+      }
+      return true;
+    }
+
+    this.router.navigate(['/']);
+    return false;
+  }
+}
